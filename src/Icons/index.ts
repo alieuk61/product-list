d2f3a95978fb661aa517807c4e4186f8e5f47a68
selly361 <seikou615@gmail.com>
2024-07-12 00:52:08 +0100

    Export svg icons

---
+++ src/Icons/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as IconAddToCart } from './icon-add-to-cart.svg'
+export { ReactComponent as IconCarbonNeutral } from './icon-carbon-neutral.svg'
+export { ReactComponent as IconDecrementQuantity } from './icon-decrement-quantity.svg'
+export { ReactComponent as IconIncrementQuantity } from './icon-increment-quantity.svg'
+export { ReactComponent as IconOrderConfirmed } from './icon-order-confirmed.svg'
+export { ReactComponent as IconRemoveItem } from './icon-remove-item.svg'
+export { ReactComponent as IconIllustrationEmptyCart } from './illustration-empty-cart.svg'
