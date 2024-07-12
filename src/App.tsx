5a0f20811803dca48b71d850e6511d943ef7ed20
selly361 <seikou615@gmail.com>
2024-07-12 03:00:51 +0100

    Update markup with DessertList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { DessertList } from 'Components'
+
 function App() {
-	return <main className='w-screen h-screen bg-rose-50 pt-20 pl-28'></main>
+	return (
+		<main className='min-h-screen bg-rose-50 pt-20 pl-28 mb-20'>
+			<div className='w-max h-max flex flex-col gap-8'>
+				<h2 className='text-heading-l'>Desserts</h2>
+				<DessertList />
+			</div>
+		</main>
+	)
 }
 
 export default App
